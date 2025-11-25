7764d5f9d2477d7422c4e25064030eca159977b0
Muhammad Bilal <withbilal777@gmail.com>
2025-11-25 21:44:04 +0500

    welcome and button components created seprately

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css";
-
+import { Welcome } from "./Welcome";
+import { Button } from "./Button";
 //creating components to render in parent node (App)
 //components are functions for displaying the ui using html(jsx)
 
-function Welcome() {
-  return <h2>Welcome to learning React Core Concepts</h2>;
-}
-function Button() {
-  return <button>Click me</button>;
-}
 function App() {
   return (
     <div>
---
+++ src/Welcome.jsx
@@ -0,0 +1,3 @@
+export const Welcome = () => {
+  return <h2>Welcome to learning React Core Concepts</h2>;
+};
